cb0a70d4c3b9dc6a910f3ad866d082e09780244e
CariLess <90604545+CariLess@users.noreply.github.com>
2022-04-26 16:31:21 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import HomeScreen from './screens/Home';
+import IssLocationScreen from './screens/IssLocation';
+import MeteorScreen from './screens/Meteor';
+import UpdateScreen from './screens/Update';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <HomeScreen/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
