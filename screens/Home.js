353efa86a78bfdc5222374cab44914105e02f151
CariLess <90604545+CariLess@users.noreply.github.com>
2022-04-28 16:37:30 -0500

    Add files via upload

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,77 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, 
+    View, 
+    StyleSheet, 
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    TouchableOpacity, 
+    ImageBackground,
+    Image
+} from 'react-native';
 
 export default class HomeScreen extends Component{
 
     render(){
         return(
-            <View style={{
-                flex:1,
-                justifyContent:"center",
-                alignItems:"center"
-            }}>
-                <Text>HomeScreen</Text>
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+
+                <View style={styles.titleBar}>
+                <Text style={styles.titleText}>ISS Location</Text>
+                </View>
+
+                <TouchableOpacity style={styles.routeCard} >
+                    <Text style={styles.routeText} > ISS Location</Text>
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={styles.routeCard} >
+                    <Text style={styles.routeText} > Meteors</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.routeCard} >
+                    <Text style={styles.routeText} > Updates</Text>
+                </TouchableOpacity>
+
             </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+container: {
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+},
+droidSafeArea:{
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+},
+titleBar: {
+    flex: 0.15,
+    justifyContent: "center",
+    alignItems: "center"
+},
+titleText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "white"
+},
+routeCard: {
+    flex: 0.25,
+    marginLeft: 50,
+    marginRight:50,
+    marginTop: 50, 
+    borderRadius: 30,
+    backgroundColor: 'white',
+},
+routeText:{
+    fontSize: 35,
+    fontWeight: "bold",
+    color: "black",
+    marginTop: 40,
+   // paddingLeft: 30
+}
+
+})
